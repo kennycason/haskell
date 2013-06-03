84f87e4fde00de7caa29d360739bec6724d7a2f9
Kenny Cason <kenneth.cason@gmail.com>
2013-06-03 01:33:52 -0500

    b-tree insert

--- binary_tree.hs
+++ binary_tree.hs
@@ -8,45 +8,57 @@ size :: Tree -> Int
 size Null = 0
 size node = 1 + size (left node) + size (right node)
 
-insert :: Int -> Tree -> Tree
-insert _ Null = Null
-insert v node
-    | v < (value node) = if (left node) == Null 
-                         then Node (Node v Null Null) (right node)
-                         else insert v (left node)
-    | v > (value node) = if (right node) == Null 
-                         then Node (value node) (left node) (Node v Null Null)
-                         else insert v (right node)
-    | otherwise = node
+add :: Int -> Tree -> Tree
+add v Null = (Node v Null Null)
+add v node
+    | v > (value node) = Node (value node) (left node) (add v (right node)) -- insert into right subtree
+    | v < (value node)  = Node (value node) (add v (left node)) (right node) -- insert into left subtree
+    | otherwise = node -- value is(value node)new so add it
 
+-- shorter way
+-- add v node @ (Node value left right) 
+--     | v > value = Node value left (add v right) -- insert into right subtree
+--     | v < value  = Node value (add v left) right -- insert into left subtree
+--     | otherwise = node -- value is new so add it
 
 main = do 
-	let t = Node 10 Null Null
-	print (t)
+    let t = Node 10 Null Null
+    print "total:"
     print (total t)
-	print (size t)
+    print "size:"
+    print (size t)
+    print t
 
-	let t2 = insert 5 t
-	print (total t2)
+    let t2 = add 5 t
+    print "add 5";
+    print "total:" 
+    print (total t2)
+    print "size:"
     print (size t2)
-	print t2
+    print t2
 
-	let t3 = insert 20 t2
-	print (total t3)
+    let t3 = add 20 t2
+    print "add 20";
+    print "total:" 
+    print (total t3)
+    print "size:"
     print (size t3)
-	print t3
+    print t3
 
-	let t4 = insert 3 t2
-	print (total t4)
+    let t4 = add 3 t3
+    print "add 3";
+    print "total:" 
+    print (total t4)
+    print "size:"
     print (size t4)
-	print t4
+    print t4
 
-	-- more complicated test
-	-- let tc = Node 5
-	-- 			(Node 15 Null Null) 
-	-- 			(Node 10 
-	-- 				Null 
-	-- 				(Node 3 Null Null)) 
-	-- print (total tc)
-	-- print tc
+    -- more complicated test
+    -- let tc = Node 5
+    --             (Node 15 Null Null) 
+    --            (Node 10 
+    --                Null 
+    --                (Node 3 Null Null)) 
+    -- print (total tc)
+    -- print tc
 
