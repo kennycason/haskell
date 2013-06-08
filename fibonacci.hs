b9a0ae80dfa6ed266d91b517e6c151a979424820
Kenny Cason <kenneth.cason@gmail.com>
2013-06-08 00:12:49 -0500

    memoization

--- fibonacci.hs
+++ fibonacci.hs
@@ -1,3 +1,4 @@
+-- this method is slow, checkout memoization.hs for better versions
 fib 0 = 1
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
---
+++ memoization.hs
@@ -0,0 +1,17 @@
+
+slow_fib :: Int -> Integer
+slow_fib 0 = 0
+slow_fib 1 = 1
+slow_fib n = slow_fib (n-2) + slow_fib (n-1)
+
+memoized_fib :: Int -> Integer
+memoized_fib = (map fib [0 ..] !!)
+   where fib 0 = 0
+         fib 1 = 1
+         fib n = memoized_fib (n-2) + memoized_fib (n-1)
+
+main = do
+    print "start"
+    print (slow_fib 37)
+    print (memoized_fib 37)
+    print "done"
